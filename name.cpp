75d4d64b0f7072221b6ab76ffe5bc94cba2ece1c
Muhammad Yasir Asad <119122367+Muhammad-Yasir-Asad@users.noreply.github.com>
2024-09-15 09:37:42 -0700

    Add files via upload

---
+++ name.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+
+cout << "  ## "  << endl;
+cout << "##  ##" << endl;
+cout << "######" << endl;
+cout << "##  ##" << endl;
+cout << "##  ##" << endl;
+cout << "\n    " << endl;
+
+cout << "##   ## " << endl;
+cout << "## ##   " << endl;
+cout << "###     " << endl;
+cout << "## ##   " << endl;
+cout << "##   ## " << endl;
+cout << "\n";
+
+cout << "######" << endl;
+cout << "    ##" << endl;
+cout << "   ## " << endl;
+cout << "  ##  " << endl;
+cout << " ##   " << endl;
+cout << "##    " << endl;
+cout << "######" << endl;
+cout << "\n";
+
+cout << "##   ## " << endl;
+cout << "## ##   " << endl;
+cout << "###     " << endl;
+cout << "## ##   " << endl;
+cout << "##   ## " << endl;
+cout << "\n";
+
+cout << " ####  " << endl;
+cout << "##  ## " << endl;
+cout << "##     " << endl;
+cout << "  #### " << endl;
+cout << "     ##" << endl;
+cout << "##   ##" << endl;
+cout << " ##### " << endl;
+
+
+
+
+}
