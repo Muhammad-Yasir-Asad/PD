4f73711e8bba574ffbe4823cb304fa0fa83b9455
Muhammad Yasir Asad <119122367+Muhammad-Yasir-Asad@users.noreply.github.com>
2024-09-21 16:48:47 +0500

    Add files via upload

---
+++ PD-02/cow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+
+	cout << "  -----------------         " << endl;
+	cout << "    o   ^__^                " << endl;
+	cout << "     o  (oo)\\________      " << endl;
+	cout << "        (__)\\       )\\/\\ " << endl;
+	cout << "            ||      ||      " << endl;
+	cout << "            ||      ||      " << endl;
+
+}
---
+++ PD-02/pistol.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+
+	system("color 09");
+	cout << " +--^----------,---------,----,---------^-, " << endl;
+	cout << "  | |||||||||    '---------'     |         o" << endl;
+	cout << "  '+-----------------------------^---------|" << endl;
+	cout << "    ,------------,---------,-------------'  " << endl;
+	cout << "     // xxxxxxx //'|       /                " << endl;
+	cout << "    // xxxxxxx //  | )    /                 " << endl;
+	cout << "   // xxxxxxx // '------'/                  " << endl;
+	cout << "  // xxxxxxx //                             " << endl;
+	cout << " // xxxxxxx //                              " << endl;
+	cout << "(----------(                                " << endl;
+
+}
