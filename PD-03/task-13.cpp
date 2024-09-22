d7ae07c5c197c5c9f0444ac0a438b3d7a71a83fc
Muhammad Yasir Asad <119122367+Muhammad-Yasir-Asad@users.noreply.github.com>
2024-09-22 00:44:54 -0700

    task-13.cpp

--- PD-03/task-13.cpp
+++ PD-03/task-13.cpp
@@ -4,18 +4,23 @@ using namespace std;
 main()
 {
 
-	int integer_1[3];
-	int integer_2[2];
-	cout << "Enter the first integer : ";
-	cin >> integer_1[1];
-	cout << "Enter the second integer : ";
-	cin >> integer_1[2];
-	cout << "Enter the third integer : ";
-	cin >> integer_1[3];
-	cout << "Enter the fourth integer : ";
-	cin >> integer_2[1];
-	cout << "Enter the fifth integer : ";
-	cin >> integer_2[2];
-	int sum = integer_1[1] + integer_1[2] + integer_1[3] + integer_2[1] + integer_2[2];
+	int sum = 0;
+	int number;
+	cout << "Enter the first number : ";
+	cin >> number;
+	sum = sum + number;
+	cout << "Enter the second number : ";
+	cin >> number;
+	sum = sum + number;
+	cout << "Enter the second number : ";
+	cin >> number;
+	sum = sum + number;
+	cout << "Enter the second number : ";
+	cin >> number;
+	sum = sum + number;
+	cout << "Enter the second number : ";
+	cin >> number;
+	sum = sum + number;
 	cout << "The sum of all integers is : " << sum;
+
 }
