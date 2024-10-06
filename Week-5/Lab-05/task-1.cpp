86b97187c09c312e49d66ec07e88bc3a8924eefe
Muhammad Yasir Asad <119122367+Muhammad-Yasir-Asad@users.noreply.github.com>
2024-10-06 04:15:44 -0700

    task-1.cpp

--- Week-5/Lab-05/task-1.cpp
+++ Week-5/Lab-05/task-1.cpp
@@ -6,14 +6,14 @@ int minimum(int, int);    //Function Declaration
 main()
 {
 
-    float min;    //varaible 
-    float num1;
-    float num2;
+    float min;    //varaible declaration
+    float num1;   //varaible declaration 
+    float num2;   //varaible declaration 
     cout << "Enter the first number : ";
     cin >> num1;
     cout << "Enter the second number : ";
     cin >> num2;
-    min = minimum(num1, num2);
+    min = minimum(num1, num2);        //pass value to function and stores return value in variable
     cout << "The minimum of " << num1 << " and " << num2 << " is : " << min;
     
 }
